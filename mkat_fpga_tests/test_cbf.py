cddf7846c04cfe3ed3e255deb93dc9c920197d6b
MphoM <mphego@ska.ac.za>
2015-07-27 15:19:18 +0200

    mathematic logic corrected.

--- mkat_fpga_tests/test_cbf.py
+++ mkat_fpga_tests/test_cbf.py
@@ -413,10 +413,9 @@ class test_CBF(unittest.TestCase):
                 s0 = scans[0][freq_i]
                 s1 = scans[scan_i]
                 norm_fac = initial_max_freq_list[freq_i]
-                self.assertLess(np.abs(s1 - s0)/norm_fac, self.threshold,
+                self.assertLess(np.abs(np.max(s1) - np.max(s0))/norm_fac), self.threshold,
                     'frequency scan comparison({}) is >= {} threshold[dB].'
                         .format(np.abs(s1 - s0)/norm_fac, self.threshold))
-        import IPython; IPython.embed()
 
     @unittest.skip('Correlator startup is currently unreliable')
     def test_restart_consistency(self):
