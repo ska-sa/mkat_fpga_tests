4ddf9914e79d28bb30eb8e809f6942336fb852c5
MphoM <mphego@ska.ac.za>
2015-07-31 09:42:54 +0200

    added bit of cmc execution commands.

--- mkat_fpga_tests/__init__.py
+++ mkat_fpga_tests/__init__.py
@@ -30,6 +30,9 @@ class CorrelatorFixture(object):
             if int(test_config.get('start_correlator', False)):
                 # Is it not easier to just call a self._correlator method?
                 self.start_correlator()
+
+            # get config file from /etc/corr/{array-name}-{instrument-name}, e.g.
+            # /etc/corr/array0-c8n856M4k
             self._correlator = fxcorrelator.FxCorrelator(
                 'test correlator', config_source=self.config_filename)
             self.correlator.initialise(program=False)
@@ -38,11 +41,24 @@ class CorrelatorFixture(object):
     def start_stop_data(self, start_or_stop, engine_class):
         assert start_or_stop in ('start', 'stop')
         assert engine_class in ('xengine', 'fengine')
-        subprocess.check_call([
-            'corr2_start_stop_tx.py', '--{}'.format(start_or_stop),
-            '--class', engine_class])
+        # kcpcmd -s localhost:{array-port} capture-destination c856M4k 10.100.201.1
+        #subprocess.check_call(['/usr/local/bin/kcpcmd' ,'-s' ,
+            #'localhost:{}'.format(array_port) ,'capture-destination' ,
+                #'{}'.format(mode), '{}'.format(destination)])
+
+        # kcpcmd -s localhost:{array-port} capture-start c856M4k
+        # or
+        # kcpcmd -s localhost:{array-port} capture-stop c856M4k
+        #subprocess.check_call(['/usr/local/bin/kcpcmd' ,'-s' ,
+            #'localhost:{}'.format(array_port) ,'capture-{}'.format(start_or_stop) ,
+                #'{}'.format('c856M4k')]
+
+        #subprocess.check_call([
+            #'corr2_start_stop_tx.py', '--{}'.format(start_or_stop),
+            #'--class', engine_class])
 
     def start_x_data(self):
+        # On array interf
         self.start_stop_data('start', 'xengine')
 
     def stop_x_data(self):
@@ -51,7 +67,32 @@ class CorrelatorFixture(object):
     def start_correlator(self, retries=5, loglevel='INFO'):
         success = False
         retries_requested = retries
+
+        # Read deng multicast groups from config
+        # multcast_ip
+        # Build list of 8
+
+        subprocess.check_call(['/usr/local/bin/kcpcmd', '-t', '30', '-s',
+            'localhost:7147', 'array-assign', 'array{}'.format(array_no),
+                '{}'.format(multcast_ip)])
+
+        #'kcpcmd -t 30 -s localhost:7147 array-assign array0 239.0.1.68+1:8888 239.0.1.70+1:8888 239.0.1.68+1:8888 239.0.1.70+1:8888 239.0.1.68+1:8888 239.0.1.70+1:8888 239.0.1.68+1:8888 239.0.1.70+1:8888'
+
+        # kcpcmd array-assign array0 [list of deng multicast groups]
+        # Do something to get katcp port of array interface and store it
+        katcp_port = subprocess.Popen("kcpcmd -s localhost:7147 array-list array{0}\
+            | grep array{0} | cut -f3 -d ' '".format(array_no)
+                , shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
+
+
         while retries and not success:
+
+            subprocess.check_call(['kcpcmd' '-s' 'localhost:{}'.format(array_port), 'instrument-activate', '{}'.format(mode)])
+
+            subprocess.check_call(['kcpcmd', '-t', '30', '-s', 'localhost:{}'.format(port),
+                'array-assign', 'array{}'.format(array_no), '{}'.format(multicast_ip)])
+
+                        # Will create a correlator config file in
             success = 0 == subprocess.call(
                 ['corr2_startcorr.py', '--loglevel', loglevel])
             retries -= 1
