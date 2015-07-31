39094123013ebe06144b44bfe8a926289e0b565c
MphoM <mphego@ska.ac.za>
2015-07-31 16:10:34 +0200

    begin to use cmc to run tests.

--- mkat_fpga_tests/__init__.py
+++ mkat_fpga_tests/__init__.py
@@ -57,6 +57,7 @@ class CorrelatorFixture(object):
             #'corr2_start_stop_tx.py', '--{}'.format(start_or_stop),
             #'--class', engine_class])
 
+
     def start_x_data(self):
         # On array interf
         self.start_stop_data('start', 'xengine')
@@ -64,49 +65,4 @@ class CorrelatorFixture(object):
     def stop_x_data(self):
         self.start_stop_data('stop', 'xengine')
 
-    def start_correlator(self, retries=5, loglevel='INFO'):
-        success = False
-        retries_requested = retries
-
-        # Read deng multicast groups from config
-        # multcast_ip
-        # Build list of 8
-
-        subprocess.check_call(['/usr/local/bin/kcpcmd', '-t', '30', '-s',
-            'localhost:7147', 'array-assign', 'array{}'.format(array_no),
-                '{}'.format(multcast_ip)])
-
-        #'kcpcmd -t 30 -s localhost:7147 array-assign array0 239.0.1.68+1:8888 239.0.1.70+1:8888 239.0.1.68+1:8888 239.0.1.70+1:8888 239.0.1.68+1:8888 239.0.1.70+1:8888 239.0.1.68+1:8888 239.0.1.70+1:8888'
-
-        # kcpcmd array-assign array0 [list of deng multicast groups]
-        # Do something to get katcp port of array interface and store it
-        katcp_port = subprocess.Popen("kcpcmd -s localhost:7147 array-list array{0}\
-            | grep array{0} | cut -f3 -d ' '".format(array_no)
-                , shell=True, stdout=subprocess.PIPE).stdout.read().rstrip()
-
-
-        while retries and not success:
-
-            subprocess.check_call(['kcpcmd' '-s' 'localhost:{}'.format(array_port), 'instrument-activate', '{}'.format(mode)])
-
-            subprocess.check_call(['kcpcmd', '-t', '30', '-s', 'localhost:{}'.format(port),
-                'array-assign', 'array{}'.format(array_no), '{}'.format(multicast_ip)])
-
-                        # Will create a correlator config file in
-            success = 0 == subprocess.call(
-                ['corr2_startcorr.py', '--loglevel', loglevel])
-            retries -= 1
-            if success:
-                LOGGER.info('Correlator started succesfully')
-            else:
-                LOGGER.warn('Failed to start correlator, {} attempts left'
-                            .format(retries))
-
-        if not success:
-            raise RuntimeError('Could not successfully start correlator within {} retries'
-                               .format(retries_requested))
-
-    def issue_metadata(self):
-        subprocess.check_call('corr2_issue_spead_metadata.py')
-
-correlator_fixture = CorrelatorFixture()
+    def start_correlator(self, retries=5, loglevel='INFO')
