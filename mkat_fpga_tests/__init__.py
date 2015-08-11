607c9b7cf191f7ef3ce6a57a75bf53d93a907018
MphoM <mphego@ska.ac.za>
2015-08-11 10:01:31 +0200

    made few mods

--- mkat_fpga_tests/__init__.py
+++ mkat_fpga_tests/__init__.py
@@ -15,9 +15,11 @@ class CorrelatorFixture(object):
 
     def __init__(self, config_filename=None):
         if config_filename is None:
-            config_filename = os.environ['CORR2INI']
+            config_filename = utils.parse_ini_file('/etc/corr/array0-c8n856M4k',
+                                ['dsimengine'])
         self.config_filename = config_filename
-        os.environ['CORR2INI'] = config_filename
+        utils.parse_ini_file('/etc/corr/array0-c8n856M4k',
+            ['dsimengine']) = config_filename
         self._correlator = None
         """Assume correlator is already running if this flag is True.
         IOW, don't do start_correlator() if set."""
@@ -42,30 +44,29 @@ class CorrelatorFixture(object):
 
     def start_stop_data(self, start_or_stop, engine_class):
         assert start_or_stop in ('start', 'stop')
-        assert engine_class in ('xengine', 'fengine')
+        assert modes in ('c856M4k', 'c856M32k')
         # kcpcmd -s localhost:{array-port} capture-destination c856M4k 10.100.201.1
-        #subprocess.check_call(['/usr/local/bin/kcpcmd' ,'-s' ,
-            #'localhost:{}'.format(array_port) ,'capture-destination' ,
-                #'{}'.format(instrument-name), '{}'.format(destination)])
+        subprocess.check_call(['/usr/local/bin/kcpcmd' ,'-s' ,
+            'localhost:{}'.format(array_port) ,'capture-destination' ,
+                '{}'.format(instrument_name), '{}'.format(destination)])
 
-        # kcpcmd -s localhost:{array-port} capture-start c856M4k
-        # or
-        # kcpcmd -s localhost:{array-port} capture-stop c856M4k
-        #subprocess.check_call(['/usr/local/bin/kcpcmd' ,'-s' ,
-            #'localhost:{}'.format(array_port) ,'capture-{}'.format(start_or_stop) ,
-                #'{}'.format('c856M4k')]
 
-        subprocess.check_call([
-            'corr2_start_stop_tx.py', '--{}'.format(start_or_stop),
-            '--class', engine_class])
+        subprocess.check_call(['/usr/local/bin/kcpcmd' ,'-s' ,
+            'localhost:{}'.format(array_port) ,'capture-{}'.format(start_or_stop) ,
+                '{}'.format(mode)]
+
+        #subprocess.check_call([
+        #    'corr2_start_stop_tx.py', '--{}'.format(start_or_stop),
+        #    '--class', engine_class])
 
 
     def start_x_data(self):
         # On array interf
-        self.start_stop_data('start', 'xengine')
+        #self.start_stop_data('start', 'xengine')
+        self.start_stop_data('start','c856M4k')
 
     def stop_x_data(self):
-        self.start_stop_data('stop', 'xengine')
+        self.start_stop_data('stop', 'c856M4k')
 
     def start_correlator(self, retries=5, loglevel='INFO'):
         success = False
