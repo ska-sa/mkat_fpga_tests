b40d127be246cffbaeaf8196853ac1deebf0233c
MphoM <mphego@ska.ac.za>
2015-08-17 15:53:04 +0200

    Made new test_conf for tests

--- mkat_fpga_tests/__init__.py
+++ mkat_fpga_tests/__init__.py
@@ -10,53 +10,58 @@ from corr2.dsimhost_fpga import FpgaDsimHost
 from corr2 import fxcorrelator
 from corr2 import utils
 
-
-
 LOGGER = logging.getLogger(__name__)
-
 
 class CorrelatorFixture(object):
 
-    def __init__(self, config_filename=None):
+    def __init__(self, test_config_filename=None):
 
-        if config_filename is None:
-            config_filename = utils.parse_ini_file('/etc/corr/array0-c8n856M4k')
-            self.config_filename = config_filename
-            dsim_conf = (utils.parse_ini_file(os.environ['HOME'] +
-                '/src/mkat_fpga_tests/mkat_fpga_tests/config_templates/dsim_conf.ini')
-                    ['dsimengine'])
-            dig_host = dsim_conf['host']
-            d_engine = FpgaDsimHost(dig_host, config=dsim_conf)
+        if test_config_filename is None:
+            test_config_filename = os.environ['CORR2TESTINI']
+            self.dsim_conf = utils.parse_ini_file(
+                test_config_filename)['dsimengine']
+
+        self._correlator = None
+        self._dhost = None
+
+    @property
+    def dhost(self):
+        if self._dhost is not None:
+            return self._dhost
+        else:
+            dig_host = self.dsim_conf['host']
+            self._dhost = FpgaDsimHost(dig_host, config=dsim_conf)
             # Check if D-eng is running else start it.
-            if d_engine.is_running():
+            if self._dhost.is_running():
                 LOGGER.info('D-Eng is running')
             else:
                 # Programming and starting D-Eng
-                d_engine.initialise()
-                d_engine.enable_data_output(enabled=True)
-                d_engine.registers.control.write(gbe_txen=True)
-                if d_engine.is_running():
+                self._dhost.initialise()
+                self._dhost.enable_data_output(enabled=True)
+                self._dhost.registers.control.write(gbe_txen=True)
+                if self._dhost.is_running():
                     LOGGER.info('D-Eng Started succesfully')
-                    time.sleep(5)
-        self._correlator = None
-
-        """Assume correlator is already running if this flag is True.
-        IOW, don't do start_correlator() if set."""
+            return self._dhost
 
     @property
     def correlator(self):
+        LOGGER.debug('it is here')
         if self._correlator is not None:
             LOGGER.info('Correlator started succesfully')
             return self._correlator
-
         else:
             if int(test_config.get('start_correlator', False)):
-                # Is it not easier to just call a self._correlator method?
-                time.sleep(5)
                 self.start_correlator()
 
+            # We assume either start_correlator() above has been called, or
+            # the c8n856M4k instrument was started on array0 before running the
+            # test.
+
+            # TODO: hard-coded config location
+            self.config_filename = '/etc/corr/array0-c8n856M4k'
+            #self.corr_conf = utils.parse_ini_file(self.config_filename)
             self._correlator = fxcorrelator.FxCorrelator(
-                'test correlator', config_source='/etc/corr/array0-c8n856M4k')
+                'test correlator', config_source=self.config_filename)
             self.correlator.initialise(program=False)
             LOGGER.info('Correlator started succesfully')
             return self._correlator
@@ -65,7 +70,6 @@ class CorrelatorFixture(object):
         self.modes = modes
         LOGGER.info('Correlator starting to capture data.')
         assert start_or_stop in ('start', 'stop')
-        assert self.modes in ('c856M4k', 'c856M32k')
 
         destination = self.correlator.configd['xengine']['output_destination_ip']
         destination_port = self.correlator.configd['xengine']['output_destination_port']
@@ -95,6 +99,7 @@ class CorrelatorFixture(object):
         success = False
         retries_requested = retries
         array_no = 0
+        import IPython;IPython.embed()
         host_port = self.config_filename['FxCorrelator']['katcp_port']
         multicast_ip = self.config_filename['fengine']['source_mcast_ips']
         try:
