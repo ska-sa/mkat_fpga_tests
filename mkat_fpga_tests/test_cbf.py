f1ced829969c68566f2828cf6c6adb0aae4b614d
MphoM <mphego@ska.ac.za>
2015-09-11 18:05:36 +0200

    Added reinitialising the instrument

--- mkat_fpga_tests/test_cbf.py
+++ mkat_fpga_tests/test_cbf.py
@@ -712,23 +712,50 @@ class test_CBF(unittest.TestCase):
 
     @aqf_vr('TP.C.1.40')
     def test_product_switch(self):
-        """
+                """
         (TP.C.1.40) CBF Data Product Switching Time
         """
         test_name = '{}.{}'.format(strclass(self.__class__), self._testMethodName)
         # Select dsim signal output, zero all sources, output scalings to 0.5
         init_dsim_sources(self.dhost)
+        # 1. Configure one of the ROACHs in the CBF to generate noise.
         # Put some correlated noise on both outputs
         self.dhost.noise_sources.noise_corr.set(scale=0.25)
+        # 2. Configure the CBF to generate a data product, using the noise source.
+        # Which specific data product is chosen is irrelevant.
+        data_product = 2 # ('m000_x', 'm000_y')
+        minute = 1.0
+        # 3. Confirm that SPEAD packets are being produced,
+        # with the selected data product(s).
         initial_dump = self.receiver.get_clean_dump(DUMP_TIMEOUT)
-        # Data product ('m000_x', 'm000_y')
-        data_product = 2
         test_freq_dump = initial_dump['xeng_raw'][:,data_product,:]
-        import IPython;IPython.embed()
-        """
-        4. Confirm that SPEAD packets are being produced, with the selected data product(s).
-        5. Unplug one of the data cables from any one of the ROACHs. Confirm that SPEAD packets are either no longer being produced, or that the data content is at least affected.
-        6. Plug the data cable back in, and reinitialise the instrument. Confirm that SPEAD packets are being produced, with the selected data product(s).
-        7. Unplug the power cable from one of the ROACHs.
-        8. Plug the power cable back in, and reinitialise the instrument. Confirm that SPEAD packets are being produced, with the selected data product(s).
-        """
+
+        # 4. Deprogram CBF
+        for host in (self.correlator.xhosts + self.correlator.fhosts):
+            host.deprogram()
+            Aqf.is_false(host.is_running(),'{} Deprogrammed'.format(host.host))
+        # Start timer
+        initial_time = time.time()
+        # ,and confirm that SPEAD packets are either no longer
+        # being produced, or that the data content is at least affected.
+        Aqf.is_false(self.receiver.isAlive(),
+            'Check that SPEAD parkets are nolonger being produced.')
+
+        # 5. Reinitialise the instrument and repeat step 2 and 3.
+        self.correlator_fixture.halt_array()
+        self.correlator = correlator_fixture.correlator
+
+        # Confirm that SPEAD packets are being produced,
+        # with the selected data product(s).
+
+        # Stop timer.
+        final_timer = time.time()
+
+        # Data Product switching time = End time - Start time.
+        final_time =  final_timer - initial_time
+        # Confirm data product switching time is less than 60 seconds
+        # Aqf.equal(final_time, minute)
+
+        # 6. Repeat for all combinations of available data products,
+        # including the case where the "new" data product is the same as the
+        # "old" one.
