41c597c4dfdb3af2e90698a5e40025f2c9a9960d
MphoM <mpho112@gmail.com>
2015-09-17 18:19:09 +0200

    Implementing coarse delay using cmc

--- mkat_fpga_tests/test_cbf.py
+++ mkat_fpga_tests/test_cbf.py
@@ -480,10 +480,16 @@ class test_CBF(unittest.TestCase):
                 # use correlator_fixture.corr_conf[]
                 # correlator_fixture.katcp_rct.req.delays time.time+somethign
                 # See page 22 on ICD ?delays on CBF-CAM ICD
-                fengops.feng_set_delay(self.correlator, 'm000_y', delay=delay,
+                reply = correlator_fixture.katcp_rct.req.input_labels()
+                source_name = reply.arguments[1].split()
+                # Set coarse delay using cmc
+                correlator_fixture.katcp_rct.req.delays
+                # Set coarse delay using corr2 library
+                fengops.feng_set_delay(self.correlator, source_name[1], delay=delay,
                     delta_delay=0, phase_offset=0, delta_phase_offset=0,
                         ld_time=None, ld_check=True)
-
+                if delay != 0:
+                    import IPython;IPython.embed()
                 this_freq_dump = self.receiver.get_clean_dump(DUMP_TIMEOUT)
                 data = complexise(this_freq_dump['xeng_raw']
                     [:, baseline_index, :])
