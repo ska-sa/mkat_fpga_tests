f421b6abd14e8f298e87e0a9586720e0fcc74072
MphoM <mpho112@gmail.com>
2015-11-12 11:27:38 +0200

    getting host sensor status once

--- mkat_fpga_tests/test_cbf.py
+++ mkat_fpga_tests/test_cbf.py
@@ -792,10 +792,10 @@ class test_CBF(unittest.TestCase):
 
             if host_sensor.wait(True):
                 Aqf.is_true(host_sensor.get_value(), 'Check that the QDR recovered. '
-                    'Status: {} on {}.'.format(host_sensor.get_status(),xhost.host))
+                    'Status: {} on {}.'.format(host_sensor.status(),xhost.host))
             else:
                 Aqf.failed('QDR failed to recover. '
-                    'Status: {} on {}.'.format(host_sensor.get_status(),xhost.host))
+                    'Status: {} on {}.'.format(host_sensor.status(),xhost.host))
         else:
             Aqf.failed('Error counters still incrementing. QDR did not recover')
 
