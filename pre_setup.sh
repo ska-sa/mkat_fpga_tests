04d1749069121280be00a3f3a42e5da7687f214c
Neilen Marais <nmarais@ska.ac.za>
2015-11-25 13:36:38 +0200

    Use PYTHON_SRC_PACKAGES_DIR env var for pre_setup.sh script

--- pre_setup.sh
+++ pre_setup.sh
@@ -3,16 +3,12 @@
 set -e # Abort on any errors
 set -u # Throw error on uninitialized variables
 
-SRCDIR=$1			# Where source dependency repos are
 PYTHON_SRC_PACKAGES=(corr2 casperpga katcp-python)
 
 . venv/bin/activate
 
 for pkg in "${PYTHON_SRC_PACKAGES[@]}"
 do
-    cd $SRCDIR/$pkg
+    cd $PYTHON_SRC_PACKAGES_DIR/$pkg
     pip install -e .
 done
-
-
-
