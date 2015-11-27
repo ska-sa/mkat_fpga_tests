2f64402057bddb47070bc41efe5b4d507a0fece6
Neilen Marais <nmarais@ska.ac.za>
2015-11-27 10:43:25 +0200

    Add hacksauce for spead2 compile

--- pre_setup.sh
+++ pre_setup.sh
@@ -6,11 +6,20 @@ PYTHON_SRC_PACKAGES=(katcp-python casperfpga corr2)
 
 . venv/bin/activate
 
+# Special spead2 hacksauce for dbelab04 with old GCC. This will be
+# fixed properly by upgrading the test system GCC
+#
+cd "$PYTHON_SRC_PACKAGES_DIR"/spead2
+OLD_PATH="$PATH"
+export PATH=/home/paulp/opt/gcc4.9.3/bin:"${PATH}"
+#pip install -e .
+export PATH="${OLD_PATH}"
+# end hacksauce
+##
 set -u # Throw error on uninitialized variables
-
 
 for pkg in "${PYTHON_SRC_PACKAGES[@]}"
 do
-    cd $PYTHON_SRC_PACKAGES_DIR/$pkg
+    cd "${PYTHON_SRC_PACKAGES_DIR}"/"${pkg}"
     pip install -e .
 done
