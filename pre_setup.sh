1d0d4be5982113bc1669808949252d2a82fd3ffb
Neilen Marais <nmarais@ska.ac.za>
2015-11-30 16:38:16 +0200

    Add pre_pip_install.sh step for spead2 install

--- pre_setup.sh
+++ pre_setup.sh
@@ -6,16 +6,6 @@ PYTHON_SRC_PACKAGES=(katcp-python nosekatreport casperfpga corr2)
 
 . venv/bin/activate
 
-# Special spead2 hacksauce for dbelab04 with old GCC. This will be
-# fixed properly by upgrading the test system GCC
-#
-cd "$PYTHON_SRC_PACKAGES_DIR"/spead2
-OLD_PATH="$PATH"
-export PATH=/home/paulp/opt/gcc4.9.3/bin:"${PATH}"
-#pip install -e .
-export PATH="${OLD_PATH}"
-# end hacksauce
-##
 set -u # Throw error on uninitialized variables
 
 for pkg in "${PYTHON_SRC_PACKAGES[@]}"
