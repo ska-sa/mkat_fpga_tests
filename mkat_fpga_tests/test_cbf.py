f908ec876fda8d8fe407731114e05cc230105832
MphoM <mpho112@gmail.com>
2016-06-03 16:58:08 +0200

    filename error fix

--- mkat_fpga_tests/test_cbf.py
+++ mkat_fpga_tests/test_cbf.py
@@ -1457,7 +1457,7 @@ class test_CBF(unittest.TestCase):
         plot_title = 'PFB Channel Response'
         plot_filename = '{}_adjacent_channels.png'.format(self._testMethodName)
         caption = 'Sample PFB channel response between {}'.format(test_chan)
-        aqf_plot_channels(zip(channel_response_list, legends), plt_filename,
+        aqf_plot_channels(zip(channel_response_list, legends), plot_filename,
                           plot_title, log_dynamic_range=90, log_normalise_to=1,
                           caption=caption, hlines=-3)
 
