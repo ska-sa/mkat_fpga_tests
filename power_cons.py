4ba945c64525c6a37e6e6728cc8c18244bacb514
Alec Rust <alec@ska.ac.za>
2016-10-07 12:36:49 +0200

    Power pdu logger script fixed

--- power_cons.py
+++ power_cons.py
@@ -1,66 +1,80 @@
 from __future__ import division
 
-import telnetlib
+import telnetlib,time
 import os
 import sys
 import threading
+import collections
 import numpy as np
 import pandas as pd
+import csv
 from corr2.utils import parse_ini_file
 from nosekatreport import Aqf
+
 
 test_conf = parse_ini_file('./config/test_conf.ini')
 pdu_host_ips = test_conf['pdu_hosts']['pdu_ips'].split(',')
 port = test_conf['pdu_hosts']['telnet_port']
 user = test_conf['pdu_hosts']['username']
 password = test_conf['pdu_hosts']['passwd']
-pwr = 'phReading all power'
-crnt = 'phReading all current'
+pwr = 'phReading id:all power'
+crnt = 'phReading id:all current'
 
 def get_power_cons(host_IP, port=23, *args):
     def get_stdout(data, unit):
         return str([float(i.split()[1]) for i in data.splitlines()
                  if i.endswith(unit) if len(i.split()) == 3]).strip('[]')
-    tn = telnetlib.Telnet(host_IP, port, timeout=50)
-    _None = tn.read_until('User Name :', timeout=50)
-    tn.write(user + '\r\n')
     try:
-        _None = tn.read_until('kat\r\nPassword  :', timeout=50)
-    except EOFError:
-        return False
-    tn.write(password  + '\r\n')
-    _None = tn.read_until('apc>', timeout=50)
-    tn.write(pwr + '\r\n')
-    stdout = tn.read_until('apc>', timeout=50)
-    pwr_lst = get_stdout(stdout, 'kW')
-    tn.write(crnt + '\r\n')
-    stdout = tn.read_until('apc>', timeout=50)
-    curr_lst = get_stdout(stdout, 'A')
-    tn.close()
-    return {'1. PDU_host': host_IP,
-            '2. Power (kW)': pwr_lst,
-            '3. Current (A)': curr_lst
-            }
-def dataframe_csv():
-    test = []
-    for count, host in enumerate(pdu_host_ips, start=1):
-        test.append(get_power_cons(host, port, pwr, crnt))
-    dataframe = pandas.DataFrame(data=test)
-    dataframe.to_csv('test.csv', sep='\t', encoding='utf-8')
+        tn = telnetlib.Telnet(host_IP, port, timeout=5)
+    except:
+        return None
+    else:
+        try:
+            _None = tn.read_until('User Name :', timeout=5)
+            tn.write(user + '\r\n')
+            _None = tn.read_until('Password  :', timeout=5)
+            tn.write(password  + '\r\n')
+            _None = tn.read_until('apc>', timeout=5)
+            tn.write(pwr + '\r\n')
+            stdout = tn.read_until('apc>', timeout=5)
+            pwr_lst = get_stdout(stdout, 'kW')
+            tn.write(crnt + '\r\n')
+            stdout = tn.read_until('apc>', timeout=5)
+            curr_lst = get_stdout(stdout, 'A')
+            tn.close()
+            smpl_time = str(int(time.time()))
+            return [smpl_time, host_IP, curr_lst, pwr_lst]
+        except:
+            return None
 
-def get_ph_list(pdu_host):
+def write_csv():
+    with open('test.csv', 'wb') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter='\t')
+        csv_writer.writerow(['Sample Time','PDU Host IP','Phase Current','Phase Power'])
+        while True:
+            for count, host in enumerate(pdu_host_ips, start=1):
+                data = (get_power_cons(host, port, pwr, crnt))
+                if data != None:
+                    csv_writer.writerow(data)
+                    csvfile.flush()
+                print data
+
+def get_ph_list(p_hosts, count=30):
     data_list = []
-    count = 30
     while True:
         try:
-            data_list.append(get_power_cons(pdu_host, port, pwr, crnt))
+            for pdu_host in pdu_hosts:
+                data = (get_power_cons(pdu_host, port, pwr, crnt))
+                data_list.append(data)
+                print data
         except Exception:
             pass
         count -=1
         if count == 0: break
     return data_list
-
-Ph_list = get_ph_list(pdu_host_ips[0])
+write_csv()
+Ph_list = get_ph_list(pdu_host_ips,3)
+import IPython;IPython.embed()
 PhI_list = [map(float, PhI_val.values()[1].split(',')) for PhI_val in Ph_list]
 Perc_Curr_drawn_Ph = [Ph1 / (Ph1 +  Ph2 + Ph3) for Ph1, Ph2, Ph3 in PhI_list]
 
