98ca52095f5285936fa2eee3db5c61e04d05ca89
Mpho Mphego <mpho112@gmail.com>
2018-03-20 18:43:13 +0200

    - Reworked clear all delays function and disable warning and loggings function

--- scripts/generate_color.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function change_color(){
-    find . -name "*.html" -print0 | xargs -0 sed -i -e 's/class="green"/style="color: green; background-color: white;"/g'
-    find . -name "*.html" -print0 | xargs -0 sed -i -e 's/class="red"/style="color: red; background-color: white;"/g'
-    find . -name "*.html" -print0 | xargs -0 sed -i -e 's/class="gray"/style="color: gray; background-color: white;"/g'
-    find . -name "*.html" -print0 | xargs -0 sed -i -e 's/class="orange"/style="color: lightgray; background-color: white;"/g'
-}
-change_color
