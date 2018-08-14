3918fd60998689a28990624d30e8aace54fc96ba
Mpho Mphego <mpho112@gmail.com>
2018-08-14 09:43:41 +0200

    [BugFix] Issue when running setup.py install where it tries to install a Null package

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # https://stackoverflow.com/a/44077346
 import atexit
 import os
+import subprocess
 
 from contextlib import contextmanager
 from distutils.command.install import install
@@ -20,53 +21,60 @@ except ImportError:
 
 from setuptools import setup
 from setuptools import find_packages
-from subprocess import check_output
 from warnings import filterwarnings
 
 
-# I do not condone it, but we surpressing all warnings
+# I do not condone it, but we suppressing all warnings
 filterwarnings('ignore')
 
 # Latest tagged stable version
-__version__ = check_output(["git", "describe", "--tags"]).rstrip().split('-')[-1]
+try:
+    __version__ = subprocess.check_output(
+        ["git", "describe", "--tags"]).rstrip().split('-')[-1]
+except subprocess.CalledProcessError:
+    __version__ = '0.5'
 
 # Install dependencies with failover/failsafe
 try:
     install_reqs = parse_requirements("pip-requirements.txt",  session=PipSession())
     __install_requires__ = [str(ir.req) for ir in install_reqs]
     try:
-        __install_requires__.remove('None')
+        __install_requires__ = filter('None'.__ne__, __install_requires__)
     except ValueError:
         pass
 except Exception:
-    __install_requires__ = ['cryptography',
-                          'matplotlib',
-                          'memory_profiler',
-                          'ntplib',
-                          'numpy',
-                          'sphinx',
-                          'h5py',
-                          'Nose',
-                          'ansicolor',
-                          'traceback2']
+    __install_requires__ = [
+        'cryptography',
+        'matplotlib',
+        'memory_profiler',
+        'ntplib',
+        'numpy',
+        'sphinx',
+        'h5py',
+        'Nose',
+        'ansicolor',
+        'traceback2']
+
 
 @contextmanager
 def cwd(path):
-    oldpwd=os.getcwd()
+    oldpwd = os.getcwd()
     os.chdir(path)
     try:
         yield
     finally:
         os.chdir(oldpwd)
 
+
 class cmdatexit_install(install):
-  """"""
-  def run(self):
-    path = ''
-    install.run(self)
-    if os.path.exists(path):
-      with cwd(path):
-          print 'Installing %s from %s' % (path, os.getcwd())
+    """"""
+
+    def run(self):
+        path = ''
+        install.run(self)
+        if os.path.exists(path):
+            with cwd(path):
+                print 'Installing %s from %s' % (path, os.getcwd())
 
 
 setup(name='mkat_fpga_tests',
@@ -78,12 +86,12 @@ setup(name='mkat_fpga_tests',
       author_email='mmphego@ska.ac.za',
       url='https://github.com/ska-sa/mkat_fpga_tests',
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Radio Telescope correlator builders',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Topic :: Scientific/Engineering :: Astronomy',
-        ],
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Radio Telescope correlator builders',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Topic :: Scientific/Engineering :: Astronomy',
+      ],
       install_requires=__install_requires__,
       dependency_links=['https://github.com/ska-sa/nosekatreport'],
       provides=['mkat_fpga_tests'],
