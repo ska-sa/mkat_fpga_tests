0b6d3cdbe853a5144cf3a4a14f125245d38ba712
Mpho Mphego <mpho112@gmail.com>
2018-09-03 09:35:31 +0200

    Updated setup virtualenv script

--- scripts/pre_setup.sh
+++ scripts/pre_setup.sh
@@ -8,89 +8,120 @@
 
 # Author: Mpho Mphego <mmphego@ska.ac.za>
 
-# Abort on any errors
-set -e
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+CURDIR="${PWD}"
 VENV=".venv/bin/activate"
+
+VERBOSE=${1:-false}
+if [ "${VERBOSE}" = true ]; then
+    set -ex
+else
+    set -e
+fi
+
+function gprint (){
+    printf "%s$1%s\n" "${GREEN}" "${NORMAL}";
+}
+
+function rprint (){
+    printf "%s$1%s\n" "${RED}" "${NORMAL}";
+}
 
 if [ -f "${VENV}" ]; then
 
     source "${VENV}"
     # Special spead2 hacksauce for dbelab04 with old GCC. This will be
     # fixed properly by upgrading the test system GCC
-    export PATH=/opt/gcc4.9.3/bin:"${PATH}"
+    export PATH=/opt/gcc4.9.3/bin:"${PWD}/.venv/bin":"${PATH}"
 
-    declare -a PYTHON_SRC_PACKAGES=(spead2 casperfpga corr2 katcp-python nosekatreport)
+    declare -a PYTHON_SRC_PACKAGES=(spead2 casperfpga corr2 katcp-python)
     PYTHON_SRC_DIR=/usr/local/src
     SPEAD2_URL=https://pypi.python.org/packages/a1/0f/9cf4ab8923a14ff349d5e85c89ec218ab7a790adfdcbd11877393d0c5bba/spead2-1.1.1.tar.gz
     PYTHON_SETUP_FILE=setup.py
 
     function pip_installer {
-        if [ "${pkg}" = 'katcp-python' ];then
-            pkg=katcp
-        fi
+        pkg="$1"
         export PYTHON_PKG="${pkg}"
-        if python -c "import os; pypkg = os.environ['PYTHON_PKG']; __import__(pypkg)" &> /dev/null; then
-            printf 'Package already installed\n';
+
+        if [ "${pkg}" = 'katcp-python' ]; then
+            pkg="katcp"
+        fi
+
+        if $(command -v python) -c "import os; pypkg = os.environ['PYTHON_PKG']; __import__(pypkg)" &> /dev/null; then
+            gprint "${pkg} Package already installed";
         else
-            printf "Installing %s \n" "${pkg}"
-            echo "${INSTALL_DIR}"
+            gprint "Installing ${pkg} in ${INSTALL_DIR}"
             cd "${INSTALL_DIR}"
             if [ ! -f "${PYTHON_SETUP_FILE}" ]; then
-                printf "Python %s file not found!\n" "${PYTHON_SETUP_FILE}"
+                rprint "Python ${PYTHON_SETUP_FILE} file not found!\n"
                 continue
             else
-                sudo pip install -e .
+                if [ "${VERBOSE}" = true ]; then
+                    $(command -v pip) install -e .
+                else
+                    $(command -v pip) install -q -e .
+                fi
                 # NO SUDOing when automating
                 # sudo python setup.py install --force
-                printf "Successfully installed %s in %s\n" "${pkg}" "${INSTALL_DIR}"
+                gprint "Successfully installed ${pkg} in ${INSTALL_DIR}"
             fi
         fi
     }
 
     function spead2_installer {
+        pkg="$1"
         cd "${INSTALL_DIR}"
-        printf "Installing %s\n" "${pkg}"
+        gprint "Installing ${pkg}"
         # NO SUDOing when automating
         # env PATH=$PATH sudo pip install -v .
-        env PATH=$PATH pip install -e .
-        printf "Successfully installed %s in %s\n" "${pkg}" "${INSTALL_DIR}"
+        if [ "${VERBOSE}" = true ]; then
+            $(command -v pip) install -v .
+        else
+            $(command -v pip) install -q .
+        fi
+        gprint "Successfully installed ${pkg} in ${INSTALL_DIR}"
     }
 
     for pkg in "${PYTHON_SRC_PACKAGES[@]}"; do
         INSTALL_DIR="${PYTHON_SRC_DIR}"/"${pkg}"
-        if [ "${pkg}" = 'spead2' ]; then
+        printf "\n\n"
+        if python -c "import os; pypkg = os.environ['PYTHON_PKG']; __import__(pypkg)" &> /dev/null; then
+            rprint "${pkg} Package already installed";
+        elif [ "${pkg}" = 'spead2' ]; then
             if [ -d "${INSTALL_DIR}" ]; then
-                printf "%s directory exists.\n" "${pkg}"
+                gprint "${pkg} directory exists."
                 export PYTHON_PKG="${pkg}"
                 if python -c "import os; pypkg = os.environ['PYTHON_PKG']; __import__(pypkg)" &> /dev/null; then
-                    printf 'Package already installed\n';
+                    rprint '${pkg} Package already installed';
                 else
-                    spead2_installer
+                    spead2_installer "${pkg}"
                 fi
             else
-                printf "%s directory doesnt exist cloning.\n" "${pkg}"
+                rprint "${pkg} directory doesnt exist cloning."
                 mkdir -p "${INSTALL_DIR}" && cd "$_"
                 curl -s "${SPEAD2_URL}" | tar zx
                 mv "${pkg}"* "${pkg}"
                 export PYTHON_PKG="${pkg}"
                 if python -c "import os; pypkg = os.environ['PYTHON_PKG']; __import__(pypkg)" &> /dev/null; then
-                    printf 'Package already installed\n';
+                    gprint "Package ${pkg} already installed";
                 else
-                    spead2_installer
+                    spead2_installer "${pkg}"
                 fi
             fi
         elif [ -d "${INSTALL_DIR}" ]; then
-            printf "%s directory exists.\n" "${pkg}"
-            pip_installer
+            gprint "${pkg} directory exists."
+            pip_installer "${pkg}"
         else
-            printf "%s directory doesnt exist cloning.\n" "${pkg}"
-            $(which git) clone git@github.com:ska-sa/"${pkg}".git "${PYTHON_SRC_DIR}"/"${pkg}" && cd "$_"
-            pip_installer
+            rprint "${pkg} directory doesnt exist cloning."
+            $(command -v git) clone git@github.com:ska-sa/"${pkg}".git "${PYTHON_SRC_DIR}"/"${pkg}" && cd "$_"
+            pip_installer "${pkg}"
         fi
-        printf "\n"
+        $(command -v python) -c "import $pkg; print $pkg.__file__" || true
     done
-    export PATH=".venv/bin":"${PATH}"
+    cd "${CURDIR}"
     bash --rcfile "${VENV}" -i
 else
-    printf "\n VIRTUAL ENVIROMENT MISSING!!!!!!!!!!!!!\n"
+    gprint "VIRTUAL ENVIROMENT MISSING!!!!!!!!!!!!!\n"
 fi
--- scripts/setup_virtualenv.sh
+++ scripts/setup_virtualenv.sh
@@ -1,82 +1,117 @@
 #!/usr/bin/env bash
 
-# Automated system packages installer
+# Automated virtualenv and system packages installer which installs .venv in cwd
 # Note: Include this file in the jenkins job script to setup the virtualenv.
 # Author: Mpho Mphego <mmphego@ska.ac.za>
 
+function usage() {
+    echo -e "Usage: bash $0 VERBOSE SYS_PACKAGES\n
+    Automated virtualenv and system packages installer which installs on cwd
+    VERBOSE: Boolean:- if true, everything will be printed to stdout
+    SYS_PACKAGES: Boolean:- if true, virtualenv will also use system packages\n";
+    exit 1;
+}
 
-# Abort on any errors
-set -e
+if [ -z "$*" ]; then
+    usage
+fi
+
+
+# MAIN
+VERBOSE=${1:-false}
+SYS_PACKAGES=${2:-false}
+
+if [ "${VERBOSE}" = true ]; then
+    echo "Abort on any errors and verbose"
+    set -ex
+else
+    set -e
+fi
 
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 
-if [ -z "$*" ];
-    then echo -e "Usage: bash $0 SYS_PACKAGES\n
-    Automated virtualenv and system packages installer which installs on cwd
-    SYS_PACKAGES: Boolean:- if true, virtualenv will also use system packages (Default:false)\n";
-    exit 1;
-fi
+function gprint (){
+    printf "%s$1%s\n" "${GREEN}" "${NORMAL}";
+}
 
-# MAIN
-SYS_PACKAGES=${1:-false}
+function rprint (){
+    printf "%s$1%s\n" "${RED}" "${NORMAL}";
+}
 
 # ---------------------
 VIRTUAL_ENV=".venv"
-printf "${GREEN}Installing ${VIRTUAL_ENV} in current working directory${NORMAL}\n"
+
+gprint "Installing ${VIRTUAL_ENV} in current working directory"
+
 if [ "${SYS_PACKAGES}" = true ] ; then
     # Create virtual environment and include Python system packages
-    printf "${GREEN}Creating virtualenv venv directory and including system packages${NORMAL}\n"
-    $(which virtualenv) "$VIRTUAL_ENV" -q --system-site-packages
+    gprint "Creating virtualenv venv directory and including system packages"
+    if [ "${VERBOSE}" = true ]; then
+        $(command -v virtualenv) "${VIRTUAL_ENV}" --system-site-packages
+    else
+        $(command -v virtualenv) "${VIRTUAL_ENV}" -q --system-site-packages
+    fi
 else
-    printf "${GREEN}Creating virtualenv venv directory without system packages${NORMAL}\n"
-    $(which virtualenv) "$VIRTUAL_ENV" -q
+    gprint "Creating virtualenv venv directory without system packages"
+    if [ "${VERBOSE}" = true ]; then
+        $(command -v virtualenv) "${VIRTUAL_ENV}"
+    else
+        $(command -v virtualenv) "${VIRTUAL_ENV}" -q
+    fi
 fi
+
 "$VIRTUAL_ENV"/bin/pip install -q -U pip setuptools
-printf "${GREEN}Sourcing virtualenv and exporting ${VIRTUAL_ENV}/bin to PATH...${NORMAL}\n"
+gprint "Sourcing virtualenv and exporting ${VIRTUAL_ENV}/bin to PATH..."
 source "${VIRTUAL_ENV}/bin/activate"
 export PATH="${VIRTUAL_ENV}/bin:$PATH"
 
-printf "${GREEN}Confirm that you are in a virtualenv: $(which python) ${NORMAL}\n\n\n"
+gprint "Confirm that you are in a virtualenv: $(which python) \n\n"
 
 if [ -z "${VIRTUAL_ENV}" ]; then
-    printf "${RED}Could not create virtualenv: $VIRTUAL_ENV${NORMAL}\n"
+    rprint "Could not create virtualenv: $VIRTUAL_ENV"
     exit 2
 fi
 
 #
 function install_pip_requirements() {
     FILENAME=$1                  # Filename to read requirements from.
+    gprint "Installing development pip dependencies from ${FILENAME} file."
     if [ -f "$FILENAME" ]; then
-        if $SYS_PACKAGES; then
-            printf "${RED}!!! The use of $FILENAME is depricated.${NORMAL}\n"
-            printf "${RED}!!! Please put you PIP build requirements${NORMAL}\n"
-            printf "${RED}!!! into pip-dev-requirements.txt${NORMAL}\n\n"
+        if [ "${VERBOSE}" = true ]; then
+            $(command -v pip) install -r $FILENAME
+        else
+            $(command -v pip) install -q -r $FILENAME
         fi
-        pip install -r $FILENAME
     fi                           # do nothing if file is not found.
 }
 
-if "${SYS_PACKAGES}"; then
 
-    # You can ignore the https by using index-url and passing the http url as a parameter then set it as the trusted source.
-    # InsecurePlatformWarning: A true SSLContext object is not available.
-    # This prevents urllib3 from configuring SSL appropriately and may cause certain SSL connections to fail.
-    # You can upgrade to a newer version of Python to solve this.
-    pip install --quiet --upgrade pip certifi pyOpenSSL ndg-httpsclient pyasn1 'requests[security]'
-    printf "${GREEN}Installing development pip dependencies, with system site packages${NORMAL}\n"
-    install_pip_requirements pip-dev-requirements.txt
-    printf "${GREEN}DONE...${NORMAL}\n"
-
-    if [ -f "./setup.py" ]; then
-        printf "${GREEN}Installing setup.py${NORMAL}\n";
-        # Install with dependencies.
-        python setup.py install -f;
-    fi
+# You can ignore the https by using index-url and passing the http url as a parameter then set it as the trusted source.
+# InsecurePlatformWarning: A true SSLContext object is not available.
+# This prevents urllib3 from configuring SSL appropriately and may cause certain SSL connections to fail.
+# You can upgrade to a newer version of Python to solve this.
+if [ "${VERBOSE}" = true ]; then
+    $(command -v pip) install --upgrade pip certifi pyOpenSSL ndg-httpsclient pyasn1 'requests[security]'
+else
+    $(command -v pip) install --quiet --upgrade pip certifi pyOpenSSL ndg-httpsclient pyasn1 'requests[security]'
 fi
 
-# if [ -f "scripts/pre_setup.sh" ]; then
-#     printf "${GREEN}Install core dependencies, if pre_setup.sh script is available${NORMAL}\n\n"
-#     bash scripts/pre_setup.sh
-# fi
+install_pip_requirements "pip-dev-requirements.txt"
+
+if [ -f "./setup.py" ]; then
+    gprint "Installing setup.py";
+    # Install with dependencies.
+    if [ "${VERBOSE}" = true ]; then
+        python setup.py install -f;
+    else
+        python setup.py install -f > /dev/null 2>&1
+    fi
+fi
+gprint "DONE!!!!\n\n"
+
+if [ -f "scripts/pre_setup.sh" ]; then
+    gprint "Install core dependencies, if pre_setup.sh script is available..."
+    bash scripts/pre_setup.sh "${VERBOSE}"
+fi
