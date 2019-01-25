f8d1acff9d351f2c9395f0a10199b656f56ad70c
Mpho Mphego <mmphego@ska.ac.za>
2019-01-25 10:09:12 +0200

    Fix: Updated setup virtualenv script (#236)
    * Added library path
    * Added spead2.zip to repo
    * Updated travis.yml

--- scripts/setup_virtualenv.sh
+++ scripts/setup_virtualenv.sh
@@ -22,13 +22,18 @@ function rprint (){
 VIRTUAL_ENV=".venv"
 
 gprint "Installing ${VIRTUAL_ENV} in current working directory"
-$(command -v virtualenv) "${VIRTUAL_ENV}" -q
+$(command -v virtualenv) "${VIRTUAL_ENV}"
 
+"${VIRTUAL_ENV}"/bin/python -W ignore::Warning -m pip install -q -U pip setuptools wheel
 gprint "Sourcing virtualenv and exporting ${VIRTUAL_ENV}/bin to PATH..."
-PYVENV="${VIRTUAL_ENV}/bin/python"
-"${PYVENV}" -W ignore::Warning -m pip install -q -U pip setuptools wheel
-export PATH="${VIRTUAL_ENV}/bin:$PATH"
-
+source "${VIRTUAL_ENV}/bin/activate"
+if [ -d "/opt/gcc4.9.3/bin" ]; then
+    export PATH=/opt/gcc4.9.3/bin:"${VIRTUAL_ENV}/bin:$PATH"
+    export LD_LIBRARY_PATH=/opt/gcc4.9.3/lib64/:/opt/gcc4.9.3/lib32/:/usr/lib/x86_64-linux-gnu/:"${LD_LIBRARY_PATH}"
+else
+    export PATH="${VIRTUAL_ENV}/bin:$PATH"
+    export LD_LIBRARY_PATH=/usr/lib/gcc/x86_64-linux-gnu/:/usr/lib/x86_64-linux-gnu/:"${LD_LIBRARY_PATH}"
+fi
 gprint "Confirm that you are in a virtualenv: $(which python)"
 
 if [ -z "${VIRTUAL_ENV}" ]; then
@@ -37,43 +42,63 @@ if [ -z "${VIRTUAL_ENV}" ]; then
 fi
 
 #
+function pkg_checker() {
+    pkg=$1
+    if ! $(command -v python) -c "import ${pkg}; print ${pkg}.__file__" >/dev/null; then
+        echo "Failed to install ${pkg}";
+        exit 1;
+    fi
+}
+
 function install_pip_requirements() {
     FILENAME=$1                  # Filename to read requirements from.
     gprint "Installing development pip dependencies from ${FILENAME} file."
-    if [ -f "${FILENAME}" ]; then
-        "${PYVENV}" -W ignore::Warning -m pip install -q -r "${FILENAME}"
+    if [ -f "$FILENAME" ]; then
+        $(command -v python) -W ignore::Warning -m pip install --no-cache-dir --ignore-installed -r "${FILENAME}" || true
     fi
 }
 
-"${PYVENV}" -W ignore::Warning -m pip install --quiet --upgrade \
-    pip numpy certifi pyOpenSSL ndg-httpsclient pyasn1 'requests[security]'
+function pip_dependencies() {
+    $(command -v python) -W ignore::Warning -m pip install --upgrade \
+        certifi pyOpenSSL ndg-httpsclient pyasn1 'requests[security]' numpy>1.15.0
 
-env CC=$(which gcc) CXX=$(which g++) "${PYVENV}" -W ignore::Warning -m pip wheel --no-cache-dir \
-     https://github.com/ska-sa/spead2/releases/download/v1.2.0/spead2-1.2.0.tar.gz
+    # Last tested working spead2.
+    # env CC="ccache gcc" CXX="g++" $(command -v python) -W ignore::Warning -m pip wheel --no-cache-dir \
+    #     https://github.com/ska-sa/spead2/releases/download/v1.2.0/spead2-1.2.0.tar.gz
+    # if [ -f "spead2-1.2.0-cp27-cp27mu-linux_x86_64.whl" ]; then
+    #     env CC="ccache gcc" CXX="g++" $(command -v python) -W ignore::Warning -m pip install \
+    #         spead2-1.2.0-cp27-cp27mu-linux_x86_64.whl
+    # fi
+    # pkg_checker spead2
+    # cd opt/spead2
+    # env CC="ccache gcc" CXX="g++" PATH=$PATH $(command -v pip) install .
+    # cd -
+
+    # Installing nosekatreport
+    $(command -v python) -W ignore::Warning -m pip install -I \
+        git+https://github.com/ska-sa/nosekatreport.git@karoocbf#egg=nosekatreport
+
+    # Installing casperfpga
+    $(command -v python) -W ignore::Warning -m pip install -I \
+        git+https://github.com/ska-sa/casperfpga@devel#egg=casperfpga
+
+    # Installing corr2 and manually installing dependencies
+    $(command -v python) -W ignore::Warning -m pip install -I \
+        git+https://github.com/ska-sa/corr2@devel#egg=corr2
+}
 
 function post_setup(){
-    if [ -f "setup.py" ]; then
-        gprint "Installing setup.py";
-        # Install with dependencies.
-        "${PYVENV}" setup.py install -f;
-    fi
+    [ -f "setup.py" ] && $(command -v python) setup.py install -f
 }
 
-function check_packages(){
-    declare -a PYTHON_PACKAGES=(spead2 casperfpga corr2 nosekatreport)
-    for pkg in "${PYTHON_PACKAGES[@]}";do
-        gprint "Checking ${pkg} if it is installed!";
-        "${PYVENV}" -c "import ${pkg}";
-        # if [ "$?" = 0 ]; then
-        #     rprint "${pkg} is not installed"
-        #     exit 1
-        # else
-        #     exit 0
-        # fi
-
+function verify_pkgs_installed(){
+    declare -a pkgs=("corr2" "casperfpga" "katcp" "nosekatreport")
+    for pkg in "${pkgs[@]}"; do
+        pkg_checker "${pkg}";
     done
 }
-post_setup
+
+pip_dependencies
 install_pip_requirements "pip-dev-requirements.txt"
-check_packages
-rm -rf -- *.whl
+post_setup
+verify_pkgs_installed
