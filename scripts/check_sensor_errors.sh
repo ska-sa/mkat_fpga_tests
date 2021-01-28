4a34895eb91cb4533bb0aabc4415746bf0c43644
Ismail <56064610+ijassiem@users.noreply.github.com>
2021-01-28 16:50:07 +0200

    Update check_sensor_errors.sh

--- scripts/check_sensor_errors.sh
+++ scripts/check_sensor_errors.sh
@@ -2,12 +2,15 @@
 PORT=$(kcpcmd array-list | grep array-list | cut -f3 -d' ' | cut -f1 -d',')
 SENSORPORT=$(kcpcmd array-list | grep array-list | cut -f3 -d' ' | cut -f2 -d',')
 
-VAR=$(kcpcmd -s localhost:$PORT sensor-value | grep error)
-
-if [ -z "$VAR" ]
+if [ -z "$PORT" ]
 then
-      echo "NO_ERRORS"
+      echo "PORT NOT FOUND"
 else
-      echo "ERRORS: $VAR"
+      VAR=$(kcpcmd -s localhost:$PORT sensor-value | grep error)
+      if [ -z "$VAR" ]
+      then
+            echo "NO_ERRORS"
+      else
+            echo "ERRORS: $VAR"
+      fi
 fi
-
