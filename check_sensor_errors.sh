6af03b247f4fa5d26758e60a6b9bab4b52d4da4f
ijassiem <ijassiem@rts-dbec.rts.kat.ac.za>
2021-09-22 15:52:54 +0200

    Update check_sensor_errors.sh
    This script has been updated to check for sensor-value errors on the
    katcp PORT and SENSORPORT.

--- check_sensor_errors.sh
+++ check_sensor_errors.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-PORT=$(kcpcmd array-list | grep array-list | cut -f3 -d' ' | cut -f1 -d',')
-SENSORPORT=$(kcpcmd array-list | grep array-list | cut -f3 -d' ' | cut -f2 -d',')
+PORT=$(kcpcmd array-list | grep array-list | cut -f3 -d' ' | cut -f1 -d',') # stores PORT number
+SENSORPORT=$(kcpcmd array-list | grep array-list | cut -f3 -d' ' | cut -f2 -d',') # stores SENSOR PORT number
 
-if [ -z "$PORT" ]
+if [ -z "$PORT" ] # PORT number will be null if an instrument is not running
 then
-      echo "PORT NOT FOUND"
+      echo "PORT NOT FOUND" # if PORT number is null then echo PORT NOT FOUND
 else
-      VAR=$(kcpcmd -s localhost:$PORT sensor-value | grep error)
-      if [ -z "$VAR" ]
+      VAR1=$(kcpcmd -s localhost:$PORT sensor-value | grep error) # stores any sensor-value errors from PORT
+      VAR2=$(kcpcmd -s localhost:$SENSORPORT sensor-value | grep error) # stores any sensor-value errors from SENSORPORT
+      if [ -z "$VAR1" ] && [ -z "$VAR2" ]
       then
-            echo "NO_ERRORS"
+            echo "NO_ERRORS" # if no errors from PORT and SENSORPORT, echo NO_ERRORS
       else
-            echo "ERRORS: $VAR"
+            echo "ERRORS on PORT: $VAR1" # else echo errors stored in VAR1
+            echo "ERRORS on SENSORPORT: $VAR2" # else echo errors stored in VAR2
       fi
 fi
